d70db8d0dced7cd0136a873544ae65fc8221eace
MarcoMorawec <marco.morawec@gmail.com>
2013-05-20 09:08:04 -0400

    adding the init.rb file

---
+++ init.rb
@@ -0,0 +1,16 @@
+#### Surf Spot Finder ####
+
+
+## Launch this Ruby file from the command line to get started 
+
+
+APP_ROOT = File.dirname(__FILE__)
+
+#require "#{APP_ROOT}/lib/guide"
+
+require File.join(APP_ROOT, 'lib', 'guide.rb')
+
+##another option to get the App path figured out
+# $:.unshift( File.join(APP_ROOT, 'lib') )
+# require 'guide'
+
